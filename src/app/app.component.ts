c0c4d0b55ad9f9de11610f4ce97d2602afb91ba3
ringtail003 <ringtail003@gmail.com>
2019-11-10 10:02:47 +0900

    hostBindingでもホストを遡れる

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'handson-angular-css';
+
+  @HostBinding('class') get themeClass() {
+    return 'theme-funky';
+  }
 }
