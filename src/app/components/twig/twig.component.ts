11fd4bc88d3db571a375ad0596124af27f96dba5
ringtail003 <ringtail003@gmail.com>
2019-11-10 09:34:41 +0900

    子要素から自身のクラスにアクセス

--- src/app/components/twig/twig.component.ts
+++ src/app/components/twig/twig.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./twig.component.scss']
 })
 export class TwigComponent implements OnInit {
+  activeLeaf = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  activate(no: number) {
+    this.activeLeaf = no;
+  }
+
+  isActive(no: number) {
+    return no === this.activeLeaf;
   }
 
 }
